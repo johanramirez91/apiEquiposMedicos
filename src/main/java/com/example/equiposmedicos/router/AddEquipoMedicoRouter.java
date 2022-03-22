da411c1d937b179f5f705ed55c53c6cc5032edd9
Johan Ramirez <johan.ramirez@sofka.com.co>
2022-03-22 11:50:19 -0500

    Feat: router addEquipoMedico added

---
+++ src/main/java/com/example/equiposmedicos/router/AddEquipoMedicoRouter.java
@@ -0,0 +1,28 @@
+package com.example.equiposmedicos.router;
+
+import com.example.equiposmedicos.dtos.EquipoMedicoDTO;
+import com.example.equiposmedicos.usecase.AddEquipoMedicoImplement;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.MediaType;
+import org.springframework.web.reactive.function.server.RouterFunction;
+import org.springframework.web.reactive.function.server.ServerResponse;
+
+import static org.springframework.web.reactive.function.server.RequestPredicates.accept;
+import static org.springframework.web.reactive.function.server.RouterFunctions.route;
+import static org.springframework.web.reactive.function.server.RequestPredicates.POST;
+
+@Configuration
+public class AddEquipoMedicoRouter {
+
+    @Bean
+    public RouterFunction<ServerResponse> addEquipoMedico(AddEquipoMedicoImplement addEquipoMedicoImplement){
+        return route(POST("/stock/addEM").and(accept(MediaType.APPLICATION_JSON)),
+                request -> request.bodyToMono(EquipoMedicoDTO.class)
+                        .flatMap(equipoMedicoDTO -> addEquipoMedicoImplement
+                                .apply(equipoMedicoDTO)
+                                .flatMap(result -> ServerResponse.ok()
+                                        .contentType(MediaType.APPLICATION_JSON)
+                                        .bodyValue(result))));
+    }
+}
