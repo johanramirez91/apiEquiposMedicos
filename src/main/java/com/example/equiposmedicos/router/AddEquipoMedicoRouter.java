8d8c0adac91620c45f18d8b0c4c89fde8ea7940b
Johan Ramirez <johan.ramirez@sofka.com.co>
2022-03-22 12:13:09 -0500

    Test: addEM test added and passed, Fix: Router http method fixed to create

--- src/main/java/com/example/equiposmedicos/router/AddEquipoMedicoRouter.java
+++ src/main/java/com/example/equiposmedicos/router/AddEquipoMedicoRouter.java
@@ -4,6 +4,7 @@ import com.example.equiposmedicos.dtos.EquipoMedicoDTO;
 import com.example.equiposmedicos.usecase.AddEquipoMedicoImplement;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.web.reactive.function.server.RouterFunction;
 import org.springframework.web.reactive.function.server.ServerResponse;
@@ -21,7 +22,7 @@ public class AddEquipoMedicoRouter {
                 request -> request.bodyToMono(EquipoMedicoDTO.class)
                         .flatMap(equipoMedicoDTO -> addEquipoMedicoImplement
                                 .apply(equipoMedicoDTO)
-                                .flatMap(result -> ServerResponse.ok()
+                                .flatMap(result -> ServerResponse.status(HttpStatus.CREATED)
                                         .contentType(MediaType.APPLICATION_JSON)
                                         .bodyValue(result))));
     }
