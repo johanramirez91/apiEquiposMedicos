5ea631311b40330a1fbf75833498ba5a108462f0
Johan Ramirez <johan.ramirez@sofka.com.co>
2022-03-22 09:49:15 -0500

    chore: webflux and mongo configuration addded

---
+++ src/main/java/com/example/equiposmedicos/EquiposmedicosApplication.java
@@ -0,0 +1,13 @@
+package com.example.equiposmedicos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EquiposmedicosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EquiposmedicosApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/example/equiposmedicos/collections/EquipoMedico.java
@@ -0,0 +1,84 @@
+package com.example.equiposmedicos.collections;
+
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+import java.time.LocalDate;
+
+@Document
+public class EquipoMedico {
+
+    @Id
+    private String id;
+    private String nombre;
+    private String ubicacion;
+    private String marca;
+    private String serie;
+    private String descripción;
+    private LocalDate fechaMantenimiento;
+    private LocalDate fechaAdquisicion;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getUbicacion() {
+        return ubicacion;
+    }
+
+    public void setUbicacion(String ubicacion) {
+        this.ubicacion = ubicacion;
+    }
+
+    public String getMarca() {
+        return marca;
+    }
+
+    public void setMarca(String marca) {
+        this.marca = marca;
+    }
+
+    public String getSerie() {
+        return serie;
+    }
+
+    public void setSerie(String serie) {
+        this.serie = serie;
+    }
+
+    public String getDescripción() {
+        return descripción;
+    }
+
+    public void setDescripción(String descripción) {
+        this.descripción = descripción;
+    }
+
+    public LocalDate getFechaMantenimiento() {
+        return fechaMantenimiento;
+    }
+
+    public void setFechaMantenimiento(LocalDate fechaMantenimiento) {
+        this.fechaMantenimiento = fechaMantenimiento;
+    }
+
+    public LocalDate getFechaAdquisicion() {
+        return fechaAdquisicion;
+    }
+
+    public void setFechaAdquisicion(LocalDate fechaAdquisicion) {
+        this.fechaAdquisicion = fechaAdquisicion;
+    }
+}
