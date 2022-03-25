534813c0bfb3decf549a380687cf3270fff8fe3f
Johan Ramirez <johan.ramirez@sofka.com.co>
2022-03-25 16:59:29 -0500

    Fix: Cors bug fixed

--- src/main/java/com/example/equiposmedicos/EquiposmedicosApplication.java
+++ src/main/java/com/example/equiposmedicos/EquiposmedicosApplication.java
@@ -2,12 +2,29 @@ package com.example.equiposmedicos;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.reactive.config.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
+
 public class EquiposmedicosApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(EquiposmedicosApplication.class, args);
+	}
+
+	@Bean
+	public WebMvcConfigurer corsConfigurer() {
+		return new WebMvcConfigurer() {
+			public void addCorsMappings(CorsRegistry registry) {
+				registry.addMapping("/**")
+						.allowedOrigins("http://localhost:4200")
+						.allowedMethods("*")
+						.allowedHeaders("*")
+						.maxAge(3600);
+			}
+		};
 	}
 
 }
